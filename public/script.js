c3dbab6ee4d0fda3c6a7707ac4fc0cea4b5aede7
Frietvorkje69 <1028402@hr.nl>
2024-05-19 15:41:06 +0200

    bigger img

--- public/script.js
+++ public/script.js
@@ -7,15 +7,19 @@ document.getElementById('searchButton').addEventListener('click', async () => {
         // 1 in 5 chance for the Pokemon to be shiny
         const isShiny = Math.random() < 0.2;
         const imageUrl = isShiny ? data.sprites.front_shiny : data.sprites.front_default;
-        const shinyMessage = isShiny ? '<p class="text-red-500"><strong>Wait.. Is that a shiny?!</p></strong>' : '';
+        const shinyMessage = isShiny ? '<p class="text-red-500 mb-2"><strong>Wait.. Is that a shiny?!</p></strong>' : '';
+
+        const types = data.types.map(type => {
+            return type.type.name.charAt(0).toUpperCase() + type.type.name.slice(1);
+        }).join(', ');
 
         document.getElementById('pokemonData').innerHTML = `
       <h2 class="text-2xl font-bold mb-4">${data.name.toUpperCase()}'s Stats</h2>
-      <img src="${imageUrl}" alt="${data.name}" class="mx-auto mb-4">
+      <img src="${imageUrl}" alt="${data.name}" class="mx-auto mb-4" style="width: 200px; height: 200px;">
       ${shinyMessage}
       <p class="mb-2">Height: ${data.height / 10} m</p>
       <p class="mb-2">Weight: ${data.weight / 10} kg</p>
-      <p>Type: ${data.types.map(type => type.type.name).join(', ')}</p>
+      <p>Type: ${types}</p>
     `;
     } catch (error) {
         document.getElementById('pokemonData').innerHTML = '<p class="text-red-500">Pokemon not found!</p>';
